e43774b5dada34d5bdc885ff99e7d8292cfcddeb
joaopedromatias <joaopedromatias@outlook.com>
2022-10-02 15:34:49 -0300

    test: insert functional tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
+beforeEach(() => {
+  window.dataLayer = [];
+})
+
 describe('application tests', () => { 
   
   const titleText = /quiz game/i;
@@ -16,12 +20,14 @@ describe('application tests', () => {
     expect(startButton).toBeInTheDocument();
   });
 
-  it('should show the button to answer question after the play button is pressed', async () => { 
+  it('should show the loading icon after the play button is pressed', async () => { 
     render(<App/>);
 
     const startButton = screen.getByRole('button', {name: startButtonText});
-    
+
     fireEvent.click(startButton);
-  
+
+    const loadingIcon = screen.getByTestId('loading-icon');
+    expect(loadingIcon).toBeInTheDocument();
   })
 })
